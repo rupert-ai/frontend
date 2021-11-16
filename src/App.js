ff35e51aa7c693ba4467fa3d3af4498ff3f30fa2
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2021-11-16 09:57:02 +0200

    fix: remove ununsed imports

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './pages/home/Home'
 import AboutUs from './pages/about/About'
 import Contacts from './pages/contacts/Contacts'
 import NoMatch from './pages/noMatch/NoMatch';
-import Navbar from './components/navbar/Navbar'
+// import Navbar from './components/navbar/Navbar'
 import AdminDashboard from './pages/admin/Admin'
 
 function App() {
