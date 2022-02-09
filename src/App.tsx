4c08002acd1443828ec2e278f27ec1e98aed5e96
veekeys <81580355+veekeys@users.noreply.github.com>
2022-02-10 01:26:42 +0200

    set base path

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <ProvideAuth>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route
               path="/"
