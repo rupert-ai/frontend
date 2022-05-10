3dad473dbf718cac4c2ed2db6888c25ab3ccf3a2
veekeys <81580355+veekeys@users.noreply.github.com>
2022-05-10 22:03:01 +0300

    Wrap in tabs content of side panel

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,15 +1,17 @@
 import {
   ColorSwatch,
   ExpandableBlock,
+  HorizontalTabs,
   InformationPanel,
   InformationPanelBody,
   InformationPanelContent,
   InformationPanelHeader,
   Leading,
+  Tab,
   Text,
   Tooltip,
 } from "@itwin/itwinui-react";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { Ad } from "../../services/backend";
 import "./SidePanel.css";
 
@@ -21,6 +23,8 @@ type SidePanelProps = {
 };
 
 function SidePanel({ children, ad, isOpen, onClose }: SidePanelProps) {
+  const [index, setIndex] = useState(0);
+
   return (
     <>
       {children}
@@ -46,13 +50,23 @@ function SidePanel({ children, ad, isOpen, onClose }: SidePanelProps) {
           </div>
         </InformationPanelHeader>
         <InformationPanelBody>
-          <Leading>Text</Leading>
-          <InformationPanelContent>
-            {ad?.vision.fullTextAnnotation.pages?.map((page, pageIndex) => {
-              return (
-                <ExpandableBlock title={`Page ${pageIndex}`}>
-                  Confidence {page.confidence}
-                  {/* {page.blocks?.map((block, blockIndex) => {
+          <HorizontalTabs
+            labels={[
+              <Tab key="Details" label="Details" />,
+              <Tab key="Statistics" label="Statistics" />,
+            ]}
+            onTabSelected={setIndex}
+          >
+            {index === 0 && (
+              <>
+                <Leading>Text</Leading>
+                <InformationPanelContent>
+                  {ad?.vision.fullTextAnnotation.pages?.map(
+                    (page, pageIndex) => {
+                      return (
+                        <ExpandableBlock title={`Page ${pageIndex}`}>
+                          Confidence {page.confidence}
+                          {/* {page.blocks?.map((block, blockIndex) => {
                           return (
                             <ExpandableBlock title={`Block ${blockIndex}`}>
                               {block.paragraphs?.map((par) => (
@@ -61,92 +75,104 @@ function SidePanel({ children, ad, isOpen, onClose }: SidePanelProps) {
                             </ExpandableBlock>
                           );
                         })} */}
-                </ExpandableBlock>
-              );
-            })}
-            <Text>Full text: ${ad?.vision.fullTextAnnotation.text}</Text>
-          </InformationPanelContent>
-          <hr />
-          <InformationPanelContent>
-            <Leading>Colors</Leading>
-            <div style={{ display: "flex", gap: 2, flexWrap: "wrap" }}>
-              {ad?.vision.imagePropertiesAnnotation.dominantColors.colors?.map(
-                (color) => {
-                  return (
-                    <Tooltip content={color.score}>
-                      <ColorSwatch
-                        color={{
-                          r: color.color.red,
-                          g: color.color.green,
-                          b: color.color.blue,
-                          a: color.color.alpha,
-                        }}
-                      />
-                    </Tooltip>
-                  );
-                }
-              )}
-            </div>
-          </InformationPanelContent>
-          <hr />
-          <InformationPanelContent>
-            <Leading>Faces</Leading>
-            {ad?.vision.faceAnnotations?.map((face, faceIndex) => {
-              return (
-                <>
-                  <Leading>Face #{faceIndex}:</Leading>
-                  <div>Joy: {face.joyLikelihood}</div>
-                  <div>Sorrow: {face.sorrowLikelihood}</div>
-                  <div>Anger: {face.angerLikelihood}</div>
-                  <div>Surprise: {face.surpriseLikelihood}</div>
-                  <div>Exposed: {face.underExposedLikelihood}</div>
-                  <div>Blurred: {face.blurredLikelihood}</div>
-                  <div>Headwear: {face.headwearLikelihood}</div>
-                  <div style={{ display: "flex", gap: 2 }}>
-                    <span>Roll: {face.rollAngle}</span>
-                    <span>Tilt: {face.tiltAngle}</span>
-                    <span>Pan: {face.panAngle}</span>
+                        </ExpandableBlock>
+                      );
+                    }
+                  )}
+                  <Text>Full text: ${ad?.vision.fullTextAnnotation.text}</Text>
+                </InformationPanelContent>
+                <hr />
+                <InformationPanelContent>
+                  <Leading>Colors</Leading>
+                  <div style={{ display: "flex", gap: 2, flexWrap: "wrap" }}>
+                    {ad?.vision.imagePropertiesAnnotation.dominantColors.colors?.map(
+                      (color) => {
+                        return (
+                          <Tooltip content={color.score}>
+                            <ColorSwatch
+                              color={{
+                                r: color.color.red,
+                                g: color.color.green,
+                                b: color.color.blue,
+                                a: color.color.alpha,
+                              }}
+                            />
+                          </Tooltip>
+                        );
+                      }
+                    )}
                   </div>
-                  <div>Confidence: {face.detectionConfidence}</div>
-                </>
-              );
-            })}
-          </InformationPanelContent>
-          <hr />
-          <InformationPanelContent>
-            <Leading>Localized objects</Leading>
-            {ad?.vision.localizedObjectAnnotations?.map((obj) => {
-              return (
-                <div>
-                  {obj.name}: {obj.score}
-                </div>
-              );
-            })}
-          </InformationPanelContent>
-          <hr />
-          <InformationPanelContent>
-            <Leading>Labels</Leading>
-            <div style={{ display: "flex", gap: 4, flexDirection: "column" }}>
-              {ad?.vision.labelAnnotations?.map((label) => {
-                return (
-                  <div>
-                    {label.description}: {label.score}
+                </InformationPanelContent>
+                <hr />
+                <InformationPanelContent>
+                  <Leading>Faces</Leading>
+                  {ad?.vision.faceAnnotations?.map((face, faceIndex) => {
+                    return (
+                      <>
+                        <Leading>Face #{faceIndex}:</Leading>
+                        <div>Joy: {face.joyLikelihood}</div>
+                        <div>Sorrow: {face.sorrowLikelihood}</div>
+                        <div>Anger: {face.angerLikelihood}</div>
+                        <div>Surprise: {face.surpriseLikelihood}</div>
+                        <div>Exposed: {face.underExposedLikelihood}</div>
+                        <div>Blurred: {face.blurredLikelihood}</div>
+                        <div>Headwear: {face.headwearLikelihood}</div>
+                        <div style={{ display: "flex", gap: 2 }}>
+                          <span>Roll: {face.rollAngle}</span>
+                          <span>Tilt: {face.tiltAngle}</span>
+                          <span>Pan: {face.panAngle}</span>
+                        </div>
+                        <div>Confidence: {face.detectionConfidence}</div>
+                      </>
+                    );
+                  })}
+                </InformationPanelContent>
+                <hr />
+                <InformationPanelContent>
+                  <Leading>Localized objects</Leading>
+                  {ad?.vision.localizedObjectAnnotations?.map((obj) => {
+                    return (
+                      <div>
+                        {obj.name}: {obj.score}
+                      </div>
+                    );
+                  })}
+                </InformationPanelContent>
+                <hr />
+                <InformationPanelContent>
+                  <Leading>Labels</Leading>
+                  <div
+                    style={{ display: "flex", gap: 4, flexDirection: "column" }}
+                  >
+                    {ad?.vision.labelAnnotations?.map((label) => {
+                      return (
+                        <div>
+                          {label.description}: {label.score}
+                        </div>
+                      );
+                    })}
                   </div>
-                );
-              })}
-            </div>
-          </InformationPanelContent>
-          <hr />
-          <InformationPanelContent>
-            <Leading>Safe search</Leading>
-            <div style={{ display: "flex", gap: 4, flexDirection: "column" }}>
-              <div>Adult: {ad?.vision.safeSearchAnnotation.adult}</div>
-              <div>Medical: {ad?.vision.safeSearchAnnotation.medical}</div>
-              <div>Racy: {ad?.vision.safeSearchAnnotation.racy}</div>
-              <div>Spoof: {ad?.vision.safeSearchAnnotation.spoof}</div>
-              <div>Violence: {ad?.vision.safeSearchAnnotation.violence}</div>
-            </div>
-          </InformationPanelContent>
+                </InformationPanelContent>
+                <hr />
+                <InformationPanelContent>
+                  <Leading>Safe search</Leading>
+                  <div
+                    style={{ display: "flex", gap: 4, flexDirection: "column" }}
+                  >
+                    <div>Adult: {ad?.vision.safeSearchAnnotation.adult}</div>
+                    <div>
+                      Medical: {ad?.vision.safeSearchAnnotation.medical}
+                    </div>
+                    <div>Racy: {ad?.vision.safeSearchAnnotation.racy}</div>
+                    <div>Spoof: {ad?.vision.safeSearchAnnotation.spoof}</div>
+                    <div>
+                      Violence: {ad?.vision.safeSearchAnnotation.violence}
+                    </div>
+                  </div>
+                </InformationPanelContent>
+              </>
+            )}
+          </HorizontalTabs>
         </InformationPanelBody>
       </InformationPanel>
     </>
