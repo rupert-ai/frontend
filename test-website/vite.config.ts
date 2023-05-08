c6e4c6e07f73e280b42e47386b5223cb7baf9f8a
nigtas <9351976+nigtas@users.noreply.github.com>
2023-05-08 23:26:43 +0300

    add 404 handler

--- test-website/vite.config.ts
+++ test-website/vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react({ include: './404.html' }), basicSsl()],
   css: {
     preprocessorOptions: {
       scss: {
-        includePaths: ["node_modules"],
+        includePaths: ['node_modules'],
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html'),
       },
     },
   },
