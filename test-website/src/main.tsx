dbcb61a1120cf3dce1ed800a29fa1b66a896589e
nigtas <9351976+nigtas@users.noreply.github.com>
2024-03-11 21:07:49 +0200

    remove

--- test-website/src/main.tsx
+++ test-website/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-import './index.css';
 import { TestPage } from './pages/TestPage';
 import { Theme } from 'carbon-components-react';
 import PrivateRoute from './PrivateRoute';
